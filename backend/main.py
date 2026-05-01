b3191fd0cf7b6e05226233d6b27791065de10fa4
mansiyadav1209 <mansiyadav12092004@gmail.com>
2026-05-01 15:34:49 +0530

    fix router import issue

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from backend.database import Base, engine
-from routers import user, project, task
+from backend.routers import user, project, task
 
 Base.metadata.create_all(bind=engine)
 
