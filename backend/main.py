f06c8143d264e87b3de535b8e9a24b7edd3ec362
mansiyadav1209 <mansiyadav12092004@gmail.com>
2026-05-01 15:31:00 +0530

    fix import error

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from database import Base, engine
+from backend.database import Base, engine
 from routers import user, project, task
 
 Base.metadata.create_all(bind=engine)
