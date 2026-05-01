1d2ef9de827e5d0658af8456b0fcbc5554e07c7c
mansiyadav1209 <mansiyadav12092004@gmail.com>
2026-05-01 16:11:46 +0530

    fic bcrypt 72 byte issue

--- backend/auth.py
+++ backend/auth.py
@@ -1,3 +1,23 @@
+# from passlib.context import CryptContext
+# from jose import jwt
+# from datetime import datetime, timedelta
+
+# SECRET_KEY = "secret"
+# ALGORITHM = "HS256"
+
+# pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+# def hash_password(password: str):
+#     return pwd_context.hash(password[:72])
+
+# def verify_password(plain, hashed):
+#     return pwd_context.verify(plain, hashed)
+
+# def create_token(data: dict):
+#     to_encode = data.copy()
+#     expire = datetime.utcnow() + timedelta(hours=10)
+#     to_encode.update({"exp": expire})
+#     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 from passlib.context import CryptContext
 from jose import jwt
 from datetime import datetime, timedelta
@@ -8,10 +28,10 @@ ALGORITHM = "HS256"
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def hash_password(password: str):
-    return pwd_context.hash(password[:72])
+    return pwd_context.hash(password[:72])   # FIX
 
 def verify_password(plain, hashed):
-    return pwd_context.verify(plain, hashed)
+    return pwd_context.verify(plain[:72], hashed)   # 🔥 VERY IMPORTANT
 
 def create_token(data: dict):
     to_encode = data.copy()
