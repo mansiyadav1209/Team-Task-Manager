e6459ac304a9ec6f6042f86acdcdc81181321d33
mansiyadav1209 <mansiyadav12092004@gmail.com>
2026-05-01 19:26:55 +0530

    update

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://nimble-fenglisu-29eaec.netlify.app"],  # allow React frontend
+    allow_origins=["majestic-daifuku-b39ae3.netlify.app"],  # allow React frontend
     allow_credentials=True,
     allow_methods=["*"],  # allow POST, GET, OPTIONS
     allow_headers=["*"],
