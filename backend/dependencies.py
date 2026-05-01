656c825e09796cbe6e914741bd9a071ec4c2e008
mansiyadav1209 <mansiyadav12092004@gmail.com>
2026-05-02 00:04:03 +0530

    update

--- backend/dependencies.py
+++ backend/dependencies.py
@@ -4,10 +4,15 @@ from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
 from database import SessionLocal
 from models import User
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
-SECRET_KEY = "secret"
-ALGORITHM = "HS256"
+
+SECRET_KEY = os.getenv("SECRET_KEY", "secret123")
+ALGORITHM = os.getenv("ALGORITHM", "HS256")
 
 def get_db():
     db = SessionLocal()
