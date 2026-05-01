83ffd945435ac6209f5fb0ed8d2988740fa946ff
mansiyadav1209 <mansiyadav12092004@gmail.com>
2026-05-01 16:19:49 +0530

    fix bcrypt 72 byte issue

--- backend/auth.py
+++ backend/auth.py
@@ -18,6 +18,26 @@
 #     expire = datetime.utcnow() + timedelta(hours=10)
 #     to_encode.update({"exp": expire})
 #     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+# from passlib.context import CryptContext
+# from jose import jwt
+# from datetime import datetime, timedelta
+
+# SECRET_KEY = "secret"
+# ALGORITHM = "HS256"
+
+# pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+# def hash_password(password: str):
+#     return pwd_context.hash(password[:72])   # FIX
+
+# def verify_password(plain, hashed):
+#     return pwd_context.verify(plain[:72], hashed)   # 🔥 VERY IMPORTANT
+
+# def create_token(data: dict):
+#     to_encode = data.copy()
+#     expire = datetime.utcnow() + timedelta(hours=10)
+#     to_encode.update({"exp": expire})
+#     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 from passlib.context import CryptContext
 from jose import jwt
 from datetime import datetime, timedelta
@@ -27,11 +47,18 @@ ALGORITHM = "HS256"
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
+
+def safe_password(password: str):
+    return password.encode("utf-8")[:72]
+
+
 def hash_password(password: str):
-    return pwd_context.hash(password[:72])   # FIX
+    return pwd_context.hash(safe_password(password))
+
 
 def verify_password(plain, hashed):
-    return pwd_context.verify(plain[:72], hashed)   # 🔥 VERY IMPORTANT
+    return pwd_context.verify(safe_password(plain), hashed)
+
 
 def create_token(data: dict):
     to_encode = data.copy()
